71db8568c438b6c14db0ef4b7e3d0c37a4d872b2
root <cheese_rush@naver.com>
2017-06-24 18:44:03 +0000

    i changed

--- lib/db_connet.php
+++
@@ -1,5 +0,0 @@
-<?php
- $conn=mysqli_connect("127.0.0.1","root","","c9") or
- die( "can't connect mysql."); 
- 
-?>
--- reservation/reservation_add.php
+++ reservation/reservation_add.php
@@ -1,9 +1,11 @@
+<div id="home"><a href="../index.php">Pension(go to main)</a></div>
+
 <?php
  $conn=mysqli_connect("127.0.0.1","root","1234","c9") or
  die( "can't connect mysql."); 
  $set = "set autocommit = 0";
  mysqli_query($conn,$set);
- $start = "begin";
+ $start = "start transaction";
  mysqli_query($conn,$start);
  
  $chil=$_POST['chil'];
@@ -19,15 +21,18 @@
  
  $price1 = $list[0]*$norm;
  $price2 = ($list[1]*$list[0]*$chil);
- $price = $price1 + $price2;
+ $price = ($price1 + $price2)*$leng;
 
- $sql2 = "insert into reservation(pension_number, room_name, date, normal, child, length, price_total)";
- $sql2 = $sql2. "values('$number','$name', '$date', '$norm','$chil', '$leng', $price)";
- mysqli_query($conn,$sql2); 
- $sql3 = "select A.name from reservation B, pension A where A.number = B.pension_number";
+ $id = $_COOKIE['userid'];
+ echo $id;
+ $sql2 = "insert into reservation(pension_number, room_name, date, normal, child, length, price_total, userid)";
+ $sql2 = $sql2. "values('$number','$name', '$date', '$norm','$chil', '$leng', $price, '$id')";
+ $sql3 = "select name from pension where number = '$number'";
  $qu = mysqli_query($conn,$sql3);
  $qqu = mysqli_fetch_row($qu);
- $date=date("Y-m-d");
+ 
+ 
+ 
 ?>
 
 <table class="table" border='1'>
@@ -50,11 +55,24 @@
  <td><?php echo $price?></td>
  </tr>
  </table>
- <?
- $set = "set autocommit = 1";
- mysqli_query($conn,$set);
- $commit = "commit";
- mysqli_query($conn,$commit);
+ <?php
+ $result = mysqli_query($conn,$sql2);
+ if($result)
+ {
+  $set = "set autocommit=1";
+  $commit = "commit";
+  mysqli_query($conn, $set);
+  mysqli_query($conn, $commit);
+  echo "success thx";
+ }
+ else {
+  $set = "set autocommit=1";
+  $roll = "rollback";
+  mysqli_query($conn, $set);
+  mysqli_query($conn, $roll);
+  echo "sorry something wrong";
+ }
+ 
  mysqli_close($conn);
  
  ?>
